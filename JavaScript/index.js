64076f9ba6267a99e7925df4d3adeaa3e9f2ac94
JohanFlotten <johanflotten@gmail.com>
2024-06-17 10:30:41 +0200

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,17 +1,19 @@
 const links = [
-    {backgroundImage: "url('../images/linkedin.png')"},
-    {backgroundImage: "url('../images/github_logo.png')"},
-    {backgroundImage: "url('../images/instagram-logo.png')"},
-    {backgroundImage: "url('../images/twitter.jpg')"},
+    { backgroundImage: "url('./images/linkedin.png')", link: "https://www.linkedin.com" },
+    { backgroundImage: "url('./images/github_logo.png')", link: "https://github.com" },
+    { backgroundImage: "url('./images/instagram-logo.png')", link: "https://www.instagram.com" },
+    { backgroundImage: "url('./images/twitter.jpg')", link: "https://twitter.com" },
 ];
 
 const socials = document.querySelector("#socials");
 
 links.forEach(link => {
-    const bubble = document.createElement("div");
+    const bubble = document.createElement("a");
     bubble.classList.add("bubble");
     bubble.style.backgroundImage = link.backgroundImage;
     bubble.style.backgroundSize = "cover";
+    bubble.href = link.link;
+    bubble.target = "_blank";
 
     socials.appendChild(bubble);
 });
